905293eefd3575bfaaae05b4263d18f1a44ea19b
James Patterson <jim@MacBook-Air.local>
2008-09-24 22:43:39 -0400

    Added some design to company index page

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,6 +5,16 @@ class Company < ActiveRecord::Base
   
   validates_presence_of   :name, :on => :create, :message => "can't be blank"
   
+  # Temporary method that outputs a randomly sized array of brands for view purposes
+  def brands
+    brands = ["Axe", "Dove", "Lux", "Pond's", "Sunsilk", "Vasoline", "Clif", "Comfort", "Sunlight", "Surf"]
+    brands[0, rand(9)+1]
+  end
+  
+  def score
+    rand(100) / 10.0
+  end
+  
   def self.suggest(q)
     q = CGI::escape(q)
     doc = Hpricot(open("http://finance.google.com/finance/match?matchtype=matchall&q=#{q}&hl=en&gl=us"))
