0f0f6e6d8b90ec53fd4c461e169fe3ee1b006270
Todd Pinkerton <todd.pinkerton@gmail.com>
2008-10-04 17:03:36 -0400

    adding restful auth and users, sessions with activation

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,14 +2,15 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include AuthenticatedSystem
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '63aceb3c6a00684ec018ed89c58d21b4'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 end
