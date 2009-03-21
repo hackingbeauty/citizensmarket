2b32dec47ade5aefe7d8546dae966dd9ac444e06
Mark Maximus Muskardin <markmuskardin@gmail.com>
2009-03-21 17:07:56 -0400

    Multiple fix to Company Issue Score

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -7,7 +7,7 @@ class Issue < ActiveRecord::Base
   # Scopes all issues within a given category
   # i.e. - Issue.issues_in_category "Society"
   named_scope :issues_in_category, lambda { |*args| {:conditions => {:category => args.first}}}
-  
+
   def name_with_category
     "#{category} - #{name}"
   end
@@ -22,7 +22,7 @@ class Issue < ActiveRecord::Base
   # Useful for rendering a grouped list of issues.
   # i.e. - {"Environment" => ["Conservation", "Biodiversity", ...], ...}
   def self.by_category
-    categories.inject({}) do |by_category, category|
+    self.categories.inject({}) do |by_category, category|
       by_category[category] = Issue.issues_in_category(category).map(&:name)
       by_category
     end
