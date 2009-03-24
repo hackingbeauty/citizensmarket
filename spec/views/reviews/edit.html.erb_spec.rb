2ea58a137947d08ea36adeeea90ec2a04a9fbb91
Luke Griffiths <wlgriffiths@gmail.com>
2009-03-24 02:12:04 -0400

    created user view specs, but somehow broke the review view specs.  too tired to understand for now

--- spec/views/reviews/edit.html.erb_spec.rb
+++ spec/views/reviews/edit.html.erb_spec.rb
@@ -2,20 +2,20 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/reviews/edit.html.erb" do
 
-  
   before(:each) do
-    assigns[:review] = @review = stub_model(Review,
+    assigns[:review] = @user = stub_model(Review,
       :new_record? => false,
       :body => "value for body",
       :status => "value for status",
       :company_id => 1,
       :user_id => 1
     )
+  
     @controller.instance_variable_set(:@url, (ActionController::UrlRewriter.new @request, {}))
     
-    template.stub!(:object_url).and_return(review_path(@review))
     template.stub!(:new_object_url).and_return(new_review_path())
     template.stub!(:edit_object_url).and_return(edit_review_path(@review))
+    
   end
 
   it "should render edit form" do
