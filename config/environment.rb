ce219f3ec428f7db3e6096a1223c341468aa3544
Diego Scataglini <diego@junivi.com>
2009-04-10 22:18:07 -0400

    added latest will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,8 @@ Rails::Initializer.run do |config|
   config.gem 'faker', :version => '0.3.1'
   config.gem 'RedCloth', :version => '4.0.3'
   config.gem 'gravtastic', :version => '2.0.0'
+  config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
