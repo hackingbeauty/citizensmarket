1fb01cf2711780b4f117b6f8e963b3ae65e1cc1b
Diego Scataglini <diego@junivi.com>
2009-04-11 01:35:55 -0400

    moved to activerecord session storage

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090411040327) do
+ActiveRecord::Schema.define(:version => 20090411053513) do
 
   create_table "admins", :force => true do |t|
     t.string   "email"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(:version => 20090411040327) do
     t.datetime "updated_at"
     t.integer  "user_id"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "user_issues", :force => true do |t|
     t.integer  "user_id"
