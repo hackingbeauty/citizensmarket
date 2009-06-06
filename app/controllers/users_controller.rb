cb4599d57c1cfc5f3ddea07d2b3f0a5e64a44791
Mark Maximus Muskardin <markmuskardin@gmail.com>
2009-06-06 14:50:48 -0400

    adding forgot password code to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def forgot
+    if request.post?
+      user = User.find_by_email(params[:user][:email])
+      if user
+        user.create_reset_code
+        flash[:forgot_password_notice] = "Reset code sent to #{user.email}"
+      else
+        flash[:forgot_password_notice] = "#{params[:user][:email]} does not exist in system"
+      end
+      render :template => '/users/forgot'
+    end
+  end
+  
+  def reset
+    @user = User.find_by_reset_code(params[:reset_code]) unless params[:reset_code].nil?
+    if request.post?
+      if @user.update_attributes(:password => params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
+        self.current_user = @user
+        @user.delete_reset_code
+        flash[:notice] = "Password reset successfully for #{@user.email}"
+        redirect_back_or_default('/')
+      else
+        render :action => :reset
+      end
+    end
+  end
+
   def suspend
     @user.suspend!
     redirect_to users_path
