b6639060e158329224fef740ee20e48fdf61f92f
Luke Griffiths <lookmeup>
2009-09-28 11:39:53 -0400

    config.gem delcarative_authorization

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,7 @@ Rails::Initializer.run do |config|
   config.gem 'gravtastic', :version => '2.0.0'
   config.gem "ambethia-smtp-tls", :lib => "smtp-tls", :source => "http://gems.github.com/"
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'stffn-declarative_authorization', :lib => 'declarative_authorization'
   
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
