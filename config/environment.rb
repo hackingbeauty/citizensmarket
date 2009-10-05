0b7953c4da2f6e7ef15fe8a41a2be81f5c86d457
Luke Griffiths <lookmeup>
2009-10-05 15:27:47 -0400

    config.gem for rspec-rails, rspec, cucumber, webrat

--- config/environment.rb
+++ config/environment.rb
@@ -11,17 +11,7 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  # See Rails::Configuration for more options.
-
-  # Skip frameworks you're not going to use. To use Rails without a database
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
-
-  # Specify gems that this application depends on.
-  # They can then be installed with "rake gems:install" on new installations.
+  
   config.gem 'resource_controller', :version => '0.5.2'
   config.gem 'active_presenter', :version => '0.0.3'
   config.gem 'rubyist-aasm', :version => '2.0.2', :lib => 'aasm', :source => 'http://gems.github.com'
@@ -35,21 +25,14 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'stffn-declarative_authorization', :lib => 'declarative_authorization'
   
-  # Only load the plugins named here, in the order given. By default, all plugins
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Make Time.zone default to the specified zone, and make Active Record store time values
-  # in the database in UTC, and return them converted to the specified local zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
+  
+  # these four lines came from The RSpec Book, page 289
+  config.gem 'rspec-rails', :lib => false
+  config.gem 'rspec', :lib => false
+  config.gem 'cucumber'
+  config.gem 'webrat'
+  
+  
   config.time_zone = 'UTC'
 
   # Your secret key for verifying cookie session data integrity.
