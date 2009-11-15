1f795d9ca3a1007564efaebe7431c655cadc4d34
Luke Griffiths <lookmeup>
2009-11-15 02:43:02 -0500

    Revert "Merge branch 'cm/master'"
    This reverts commit b7eb6bc068c7a880af16d6cc013a1c0ca1bcf97a.

--- test/performance/homepage_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test/test_helper'
-require 'performance_test_help'
-
-class HomepageTest < ActionController::PerformanceTest
-  # Replace this with your real tests.
-  def test_homepage
-    get '/'
-  end
-end
